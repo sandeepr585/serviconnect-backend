ea8a624de5416fc271717de9040e6f013873a6f7
Sandeep Reddy <muchasandeepreddy176@gmail.com>
2026-09-21 22:49:53 +0530

    Fix PostgreSQL configuration and user table mapping

--- src/main/java/com/practice/entity/User.java
+++ src/main/java/com/practice/entity/User.java
@@ -1,4 +1,5 @@
 package com.practice.entity;
+import jakarta.persistence.Table;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -6,8 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@Table(name = "app_user")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
